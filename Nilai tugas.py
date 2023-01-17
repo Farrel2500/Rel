2d5fbb7317f68cb020e71c2485f3cd61caa7d94a
Farrel2500 <122343284+Farrel2500@users.noreply.github.com>
2023-01-17 14:14:43 +0700

    Add files via upload

---
+++ Nilai tugas.py
@@ -0,0 +1,12 @@
+Tugas=int(input("Masukan Nilai tugas: "))
+
+if Tugas >= 80:
+    print("Nilai tugas : A")
+elif Tugas >= 70:
+    print ("Nilai tugas: B")
+elif Tugas >= 60:
+    print("Nilai tugas: C")
+elif Tugas >= 50:
+    print("Nilai tugas: D")
+else:
+    print("Masukan nilai yang benar")
